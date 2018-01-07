49494638177ec405b6ce25d3fd065e5d86460a4b
andrey.starikov <starikoff@gmail.com>
2018-01-07 17:27:35 +0100

    cleanup

--- src/test/java/ru/ra/DozerTest.java
+++ src/test/java/ru/ra/DozerTest.java
@@ -4,8 +4,6 @@ import org.dozer.DozerBeanMapper;
 import org.dozer.Mapper;
 import org.junit.Test;
 
-import java.util.Arrays;
-
 public class DozerTest {
     @Test
     public void testApp() {
@@ -14,9 +12,6 @@ public class DozerTest {
         Src src = new Src();
         src.setColumns(new byte[][]{new byte[]{1, 2, 3}});
 
-        System.out.println(Arrays.deepToString(src.getColumns()));
-
         Dst dst = mapper.map(src, Dst.class);
-        System.out.println(Arrays.deepToString(dst.getColumns()));
     }
 }
